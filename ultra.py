d3b26e122f57f31977eac8e112b65c0c23199f37
sckimosu <sckim7@kookmin.ac.kr>
2017-09-30 10:39:12 +0900

    Add ultra.py

--- ultra.py
+++ ultra.py
@@ -1,17 +1,16 @@
-import RPi.GPIO as GPIO
-import time  
+import RPi.GPIO as GPIO # import GPIO librery
 from time import sleep
+import time
 
 def ultra():
-
     GPIO.setmode(GPIO.BOARD)
 
     trig=33
     echo=31
 
+    #ultrasonic sensor setting
     GPIO.setup(trig,GPIO.OUT)
     GPIO.setup(echo,GPIO.IN)
-    GPIO.setwarnings(False)
 
     try:
         while True:
@@ -21,19 +20,17 @@ def ultra():
             time.sleep(0.00001)
             GPIO.output(trig,False)
             while GPIO.input(echo)==0:
-                pulse_start=time.time()
+                pulse_start=time.time()    
             while GPIO.input(echo)==1:
                 pulse_end=time.time()
             pulse_duration=pulse_end-pulse_start
             distance=pulse_duration*17000
-             
+            distance=round(distance,2)
             print(distance)
       
     except KeyboardInterrupt:
         GPIO.cleanup()
 
-if __name__ == "__main__":
-    ultra()
-
-
-
+#if __name__=="__main__":
+#    ultra()
+    
