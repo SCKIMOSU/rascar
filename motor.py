80bb04cb8fc6f1f8f1cb8c858ef5cbb239a7c70a
sckimosu <sckim7@kookmin.ac.kr>
2017-10-08 08:40:42 +0900

    Add motor.py

--- motor.py
+++ motor.py
@@ -1,52 +1,145 @@
-import RPi.GPIO as GPIO # import GPIO librery
-import time
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import PCA9685 as p
+import time    # Import necessary modules
 
-GPIO.setmode(GPIO.BOARD)
+# ===========================================================================
+# Raspberry Pi pin11, 12, 13 and 15 to realize the clockwise/counterclockwise
+# rotation and forward and backward movements
+# ===========================================================================
+Motor0_A = 11  # pin11
+Motor0_B = 12  # pin12
+Motor1_A = 13  # pin13
+Motor1_B = 15  # pin15
 
-Motor1A = 12 #모터 1은 각각 12, 11, 35번 핀에 연결되어있다.
-Motor1B = 11
-Motor1E = 35
+# ===========================================================================
+# Set channel 4 and 5 of the servo driver IC to generate PWM, thus 
+# controlling the speed of the car
+# ===========================================================================
+EN_M0    = 4  # servo driver IC CH4
+EN_M1    = 5  # servo driver IC CH5
 
-Motor2A = 15 
-Motor2B = 13
-Motor2E = 37
+pins = [Motor0_A, Motor0_B, Motor1_A, Motor1_B]
 
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
+# ===========================================================================
+# Adjust the duty cycle of the square waves output from channel 4 and 5 of
+# the servo driver IC, so as to control the speed of the car.
+# ===========================================================================
+def setSpeed(speed):
+	speed *= 40
+	print 'speed is: ', speed
+	pwm.write(EN_M0, 0, speed)
+	pwm.write(EN_M1, 0, speed)
 
-GPIO.setup(Motor2A,GPIO.OUT)
-GPIO.setup(Motor2B,GPIO.OUT)
-GPIO.setup(Motor2E,GPIO.OUT)
+def setup(busnum=None):
+	global forward0, forward1, backward1, backward0
+	global pwm
+	if busnum == None:
+		pwm = p.PWM()                  # Initialize the servo controller.
+	else:
+		pwm = p.PWM(bus_number=busnum) # Initialize the servo controller.
 
-#initial motor forward setting 
-GPIO.output(Motor1A,GPIO.HIGH)
-GPIO.output(Motor1B,GPIO.LOW)
-GPIO.output(Motor1E,GPIO.HIGH)
-GPIO.output(Motor2A,GPIO.LOW)
-GPIO.output(Motor2B,GPIO.HIGH)
-GPIO.output(Motor2E,GPIO.HIGH)
+	pwm.frequency = 60
+	forward0 = 'True'
+	forward1 = 'True'
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BOARD)        # Number GPIOs by its physical location
+	try:
+		for line in open("config"):
+			if line[0:8] == "forward0":
+				forward0 = line[11:-1]
+			if line[0:8] == "forward1":
+				forward1 = line[11:-1]
+	except:
+		pass
+	if forward0 == 'True':
+		backward0 = 'False'
+	elif forward0 == 'False':
+		backward0 = 'True'
+	if forward1 == 'True':
+		backward1 = 'False'
+	elif forward1 == 'False':
+		backward1 = 'True'
+	for pin in pins:
+		GPIO.setup(pin, GPIO.OUT)   # Set all pins' mode as output
 
+# ===========================================================================
+# Control the DC motor to make it rotate clockwise, so the car will 
+# move forward.
+# ===========================================================================
 
-pwm=GPIO.PWM(Motor1E,100) 
-pwm2=GPIO.PWM(Motor2E,100)
+def motor0(x):
+	if x == 'True':
+		GPIO.output(Motor0_A, GPIO.LOW)
+		GPIO.output(Motor0_B, GPIO.HIGH)
+	elif x == 'False':
+		GPIO.output(Motor0_A, GPIO.HIGH)
+		GPIO.output(Motor0_B, GPIO.LOW)
+	else:
+		print 'Config Error'
 
-# starting it with 40% dutycycle
+def motor1(x):
+	if x == 'True':
+		GPIO.output(Motor1_A, GPIO.LOW)
+		GPIO.output(Motor1_B, GPIO.HIGH)
+	elif x == 'False':
+		GPIO.output(Motor1_A, GPIO.HIGH)
+		GPIO.output(Motor1_B, GPIO.LOW)
 
+def forward():
+	motor0(forward0)
+	motor1(forward1)
 
-cnt=0;
+def backward():
+	motor0(backward0)
+	motor1(backward1)
 
-try:
-    while True:
-        pwm.start(40) 
-        pwm2.start(40)
+def forwardWithSpeed(spd = 50):
+	setSpeed(spd)
+	motor0(forward0)
+	motor1(forward1)
 
+def backwardWithSpeed(spd = 50):
+	setSpeed(spd)
+	motor0(backward0)
+	motor1(backward1)
 
-except KeyboardInterrupt:
-    GPIO.output(Motor1E,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.LOW)
-    pwm.ChangeDutyCycle(0)
-    pwm2.ChangeDutyCycle(0)
-    GPIO.cleanup()
+def stop():
+	for pin in pins:
+		GPIO.output(pin, GPIO.LOW)
 
+# ===========================================================================
+# The first parameter(status) is to control the state of the car, to make it 
+# stop or run. The parameter(direction) is to control the car's direction 
+# (move forward or backward).
+# ===========================================================================
+def ctrl(status, direction=1):
+	if status == 1:   # Run
+		if direction == 1:     # Forward
+			forward()
+		elif direction == -1:  # Backward
+			backward()
+		else:
+			print 'Argument error! direction must be 1 or -1.'
+	elif status == 0: # Stop
+		stop()
+	else:
+		print 'Argument error! status must be 0 or 1.'
 
+def test():
+	while True:
+		setup()
+		ctrl(1)
+		time.sleep(3)
+		setSpeed(10)
+		time.sleep(3)
+		setSpeed(100)
+		time.sleep(3)
+		ctrl(0)
+
+if __name__ == '__main__':
+	setup()
+	setSpeed(50)
+	#forward()
+	#backward()
+	stop()
